b5740c83a81bf83990b8ca94d1577a37fa0116b0
bigyama <biggu256@gmail.com>
2021-04-01 17:10:59 +0545

    Server Authentication

---
+++ app/src/main/java/common/Common.java
@@ -0,0 +1,8 @@
+package common;
+
+import model.ServerUserModel;
+
+public class Common {
+    public static final String SERVER_REF = "Server";
+    public static ServerUserModel currentServerUser;
+}
